20ecd1f04f8b164eb7782da8d68202d4be083b24
VICTOR HUGO ROCHA DA SILVA <25132598@CFP127ALUNO.LOCAL>
2025-02-24 11:21:37 -0300

    Métodos de acesso get e set concluídos

--- src/br/sp/senai/jandira/clinica/ClinicaApp.java
+++ src/br/sp/senai/jandira/clinica/ClinicaApp.java
@@ -1,5 +1,7 @@
 package br.sp.senai.jandira.clinica;
 
+import java.time.LocalDate;
+
 import br.sp.senai.jandira.clinica.model.Paciente;
 
 public class ClinicaApp {
@@ -12,13 +14,22 @@ public class ClinicaApp {
 	p1.setNome ("Victor Hugo");
 	p1.setPeso(68);
 	p1.setAltura(1.63);
+	p1.setGenero("M");
+	p1.setTelefone("(11)994476842");
+	p1.setDataNascimento(LocalDate.of(2001, 5, 12));
 	
 	p2.setNome ("João Vitor");
 	p2.setPeso(83);
 	p2.setAltura(1.81);
+	p2.setGenero("O");
+	p2.setTelefone("192929");
 	
 	p1.exibirDados();
 	p2.exibirDados();
+	
+	System.out.println(p1.getNome().length());
+	System.out.println(p1.getNome().toUpperCase());
+	System.out.println(p1.getGenero() );
 
 	}
 
